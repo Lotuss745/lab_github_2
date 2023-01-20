e8223612f165a30ff822a2ac89d1f5cf50c8d6d7
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2023-01-20 13:58:46 +0200

    commit_message_2

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("Vieno žmogaus sukurta prasta operacinė sistema yra kito žmogaus darbas visu etatu.")
+console.log("Ant stalo stovi stiklinė, iki pusės užpildyta vandeniu. Kaip ją apibūdintų optimistas, pesimistas ir programuotojas? Štai taip. Optimistas sako: „Stiklinė pusiau pilna“, pesimistas – „Stiklinė pusiau tuščia“, o programuotojas ją apibūdina taip: „Stiklinė yra dvigubai didesnė nei turėtų būti“.")
+console.log("Programuotojas_1 - Klausyk, gal gali paskolinti 200 eurų iki atlyginimo? Programuotojas_2 – Galiu, tik suapvalinam iki 256 EUR, bus lengviau atsiminti.")
+console.log("Kodėl programuotojai kartais maišo Helovyną su Kalėdomis? Nes Oct 31 lygu Dec 25.")
